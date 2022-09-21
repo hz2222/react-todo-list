a0ea1e5c4870b0bea9935402f38cf2282924af51
hz <lebber@gmail.com>
2022-09-21 15:54:43 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+function AddTodo({onAddTodo}) {
+
+  const [newTodos, setNewTodos] = useState("");
+
+  return (
+    <div className='input-div'>
+      <input type="text" value={newTodos} onChange={e=>setNewTodos(e.target.value)}/>
+      <button className='add' onClick={() => {
+        onAddTodo(newTodos);
+        setNewTodos("");
+      }}>Add todo
+      </button>
+    </div>
+ )}
+
+function Todo({todo, onDelete}) {
+  return(
+    <div className='todos'>
+      <button className='delete' onClick={() => {
+        onDelete(todo.id);
+      }
+      }>Delete</button>
+      {todo.name}
+    </div>
+  )
+} 
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {id: 1, name:"shopping"},
+    {id: 2, name:"cooking"},
+    {id: 3, name:"go to the gym"}
+  ]);
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo list</h1>
+      {todos.map(e => {
+        return <Todo todo={e} onDelete={id=>{
+          setTodos(todos.filter(f=>f.id !== id));
+        }
+      } />
+      })}
+      <AddTodo onAddTodo={(newTodosname) => {
+        setTodos([...todos, {
+          id: `${Math.random()}`.substr(2),
+          name: newTodosname
+        }
+      ]);
+      }} />
     </div>
   );
 }
